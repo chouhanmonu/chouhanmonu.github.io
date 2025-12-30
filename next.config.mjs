12d16eb9db82254497b353ed19e510abf57c1f51
chouhanmonu <monu@greyaxis.io>
2025-12-30 11:00:11 +0530

    fix next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    qualities: [75, 100],
-  },
   output: "export", // Enables static HTML export
   images: {
+    qualities: [75, 100],
     unoptimized: true, // Required: Next.js Image optimization needs a server
   },
   trailingSlash: true, // Helps with routing on GitHub Pages
